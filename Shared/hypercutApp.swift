df8704e113e56fa5fbf25460a5bf689fd67c0717
Michael Verges <maustinstar@gmail.com>
2021-10-23 02:04:27 -0400

    Initial Commit

---
+++ Shared/hypercutApp.swift
@@ -0,0 +1,17 @@
+//
+//  hypercutApp.swift
+//  Shared
+//
+//  Created by Michael Verges on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct hypercutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
